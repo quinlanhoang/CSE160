128d398ca57fcad067ad09b150deb6a5b9cc0b82
quinlanhoang <qbhoang@ucsc.edu>
2025-05-25 16:27:47 -0700

    asg4

---
+++ asgn4/drawAllShapes.js
@@ -0,0 +1,367 @@
+var g_map=[
+   // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+   // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+   // [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+function drawMap(){
+   for(x=0; x<32; x++){
+      for(y=0; y<32; y++){
+         if ((x == 0 || x == 31) && y%4 == 0){
+            var wall = new Cube();
+            wall.color = [.80, .70, .40, 1.0];
+            wall.textureNum = -2;
+            wall.matrix.scale(0.25,0.25,0.25);
+            wall.matrix.translate(x-16,-.25,y-16);
+            wall.render();
+         }
+         if ((x == 0 || x == 31) && y%4 != 0){
+            var wall = new Cube();
+            wall.color = [.60, .40, .20, 1.0];
+            wall.textureNum = -2;
+            wall.matrix.scale(0.25,0.73,0.25);
+            wall.matrix.translate(x-16,-.25,y-16);
+            wall.render();
+         }
+         if ((y == 0 || y == 31) && x%4 == 0){
+            var wall = new Cube();
+            wall.color = [.80, .70, .50, 1.0];
+            wall.textureNum = -2;
+            wall.matrix.scale(0.25,0.25,0.25);
+            wall.matrix.translate(x-16,-.25,y-16);
+            wall.render();
+         }
+         if ((y == 0 || y == 31) && x%4 != 0){
+            var wall = new Cube();
+            wall.color = [.60, .40, .20, 1.0];
+            wall.textureNum = -2;
+            wall.matrix.scale(0.25,0.73,0.25);
+            wall.matrix.translate(x-16,-.25,y-16);
+            wall.render();
+         }
+         // if (g_map[x][y]==1){
+         //    var wall = new Cube();
+         //    wall.color = [1.0, 1.0, 1.0, 1.0];
+         //    wall.textureNum = -1;
+         //    wall.matrix.translate(x-4,-.25,y-4);
+         //    wall.render();
+         // }
+      }
+   }
+}
+
+function drawSheep(){
+   // Colors
+   var wool = [1.0, 0.75, 0.8, 1.0];    
+   var skin = [1.0, 0.87, 0.75, 1.0];   
+
+   // body =====================================
+   var body = new Cube();
+   body.color = wool;
+   body.textureNum = -2;
+   if(g_normalOn) body.textureNum = -3;
+   body.matrix.rotate(170, 0, 1, 0);
+   body.matrix.scale(.25, 0.25, 0.35);
+   body.matrix.translate(-.5, 0, -0.25);
+   body.normalMatrix.setInverseOf(body.matrix).transpose();
+   body.renderfast();
+
+   // head =====================================
+
+   var head = new Cube();
+   head.color = wool;
+   head.textureNum = -2;
+   if(g_normalOn) head.textureNum = -3;
+   head.matrix.rotate(170, 0, 1, 0);
+   head.matrix.rotate(-head_animation, 1, 0, 0);
+   head.matrix.scale(0.35, 0.35, 0.35);
+   head.matrix.translate(-.5, 0.25, -1.25);
+   head.normalMatrix.setInverseOf(head.matrix).transpose();
+   head.renderfast();
+
+   var face = new Cube();
+   face.color = skin;
+   face.textureNum = -2;
+   if(g_normalOn) face.textureNum = -3;
+   face.matrix.rotate(170, 0, 1, 0);
+   face.matrix.rotate(-head_animation, 1, 0, 0);
+   face.matrix.scale(0.30, 0.30, 0.03);
+   face.matrix.translate(-.5, 0.35, -15.5);
+   face.normalMatrix.setInverseOf(face.matrix).transpose();
+   face.renderfast();
+
+   var tophair = new Cube();
+   tophair.color = wool;
+   tophair.textureNum = -2;
+   if(g_normalOn) tophair.textureNum = -3;
+   tophair.matrix.rotate(170, 0, 1, 0);
+   tophair.matrix.rotate(-head_animation, 1, 0, 0);
+   tophair.matrix.scale(0.32, 0.071, 0.04);
+   tophair.matrix.translate(-.5, 4.85, -11.95);
+   tophair.normalMatrix.setInverseOf(tophair.matrix).transpose();
+   tophair.renderfast();
+
+   var botlefthair = new Cube();
+   botlefthair.color = wool;
+   botlefthair.textureNum = -2;
+   if(g_normalOn) botlefthair.textureNum = -3;
+   botlefthair.matrix.rotate(170, 0, 1, 0);
+   botlefthair.matrix.rotate(-head_animation, 1, 0, 0);
+   botlefthair.matrix.scale(0.05, 0.071, 0.04);
+   botlefthair.matrix.translate(-3.01, 1.5, -11.95);
+   botlefthair.normalMatrix.setInverseOf(botlefthair.matrix).transpose();
+   botlefthair.renderfast();
+
+   var botrighthair = new Cube();
+   botrighthair.color = wool;
+   botrighthair.textureNum = -2;
+   if(g_normalOn) botrighthair.textureNum = -3;
+   botrighthair.matrix.rotate(170, 0, 1, 0);
+   botrighthair.matrix.rotate(-head_animation, 1, 0, 0);
+   botrighthair.matrix.scale(0.05, 0.071, 0.04);
+   botrighthair.matrix.translate(2.01, 1.5, -11.95);
+   botrighthair.normalMatrix.setInverseOf(botrighthair.matrix).transpose();
+   botrighthair.renderfast();
+
+   var lefteye = new Cube();
+   lefteye.color = [1,1,1,1];
+   if(g_normalOn) lefteye.textureNum = -3;
+   lefteye.matrix.rotate(170, 0, 1, 0);
+   lefteye.matrix.rotate(-head_animation, 1, 0, 0);
+   lefteye.matrix.scale(0.1, 0.061, 0.04);
+   lefteye.matrix.translate(-1.5, 3.5, -11.95);
+   lefteye.normalMatrix.setInverseOf(lefteye.matrix).transpose();
+   lefteye.renderfast();
+
+   var lefteyeblack = new Cube();
+   lefteyeblack.color = [0,0,0,1];
+   if(g_normalOn) lefteyeblack.textureNum = -3;
+   lefteyeblack.matrix.rotate(170, 0, 1, 0);
+   lefteyeblack.matrix.rotate(-head_animation, 1, 0, 0);
+   lefteyeblack.matrix.scale(0.05, 0.061, 0.04);
+   lefteyeblack.matrix.translate(-3.001, 3.5, -12);
+   lefteyeblack.normalMatrix.setInverseOf(lefteyeblack.matrix).transpose();
+   lefteyeblack.renderfast();
+
+   var righteye = new Cube();
+   righteye.color = [1,1,1,1];
+   if(g_normalOn) righteye.textureNum = -3;
+   righteye.matrix.rotate(170, 0, 1, 0);
+   righteye.matrix.rotate(-head_animation, 1, 0, 0);
+   righteye.matrix.scale(0.1, 0.061, 0.04);
+   righteye.matrix.translate(0.5, 3.5, -11.95);
+   righteye.normalMatrix.setInverseOf(righteye.matrix).transpose();
+   righteye.renderfast();
+
+   var righteyeblack = new Cube();
+   righteyeblack.color = [0,0,0,1];
+   if(g_normalOn) righteyeblack.textureNum = -3;
+   righteyeblack.matrix.rotate(170, 0, 1, 0);
+   righteyeblack.matrix.rotate(-head_animation, 1, 0, 0);
+   righteyeblack.matrix.scale(0.05, 0.061, 0.04);
+   righteyeblack.matrix.translate(2.001, 3.5, -12.05);
+   righteyeblack.normalMatrix.setInverseOf(righteyeblack.matrix).transpose();
+   righteyeblack.renderfast();
+
+   var mouth = new Cube();
+   mouth.color = [1,.79,.69,1];
+   if(g_normalOn) mouth.textureNum = -3;
+   mouth.matrix.rotate(170, 0, 1, 0);
+   mouth.matrix.rotate(-head_animation, 1, 0, 0);
+   mouth.matrix.scale(0.1, 0.071, 0.04);
+   mouth.matrix.translate(-0.47, 1.5, -11.95);
+   mouth.normalMatrix.setInverseOf(mouth.matrix).transpose();
+   mouth.renderfast()
+
+   var tongue = new Cube();
+   tongue.color = [.89,.69,.64,1];
+   if(g_normalOn) tongue.textureNum = -3;
+   tongue.matrix.rotate(170, 0, 1, 0);
+   tongue.matrix.rotate(-head_animation, 1, 0, 0);
+   tongue.matrix.scale(0.1, 0.035, 0.04);
+   tongue.matrix.translate(-0.4701, 3, -12);
+   tongue.normalMatrix.setInverseOf(tongue.matrix).transpose();
+   tongue.renderfast()
+
+   // upper legs ============================
+   var frontleft = new Cube();
+   frontleft.color = wool;
+   frontleft.textureNum = -2;
+   if(g_normalOn) frontleft.textureNum = -3;
+   frontleft.matrix.setTranslate(0, 0, 0);
+   frontleft.matrix.rotate(-g_jointAngle, 0, 0, 1); // Joint 1
+   var frontleftCoord = new Matrix4(frontleft.matrix);
+   frontleft.matrix.rotate(170, 0, 1, 0);
+   frontleft.matrix.scale(.10, -0.10, 0.10);
+   frontleft.matrix.translate(-1.15, -.25, -0.75);
+   frontleft.normalMatrix.setInverseOf(frontleft.matrix).transpose();
+   frontleft.renderfast();
+
+   var frontright = new Cube();
+   frontright.color = wool;
+   frontright.textureNum = -2;
+   if(g_normalOn) frontright.textureNum = -3;
+   frontright.matrix.setTranslate(0, 0, 0);
+   frontright.matrix.rotate(g_jointAngle, 0, 0, 1); // Joint 1
+   var frontrightCoord = new Matrix4(frontright.matrix);
+   frontright.matrix.rotate(170, 0, 1, 0);
+   frontright.matrix.scale(.10, -0.10, 0.10);
+   frontright.matrix.translate(.2, -.25, -0.75);
+   frontright.normalMatrix.setInverseOf(frontright.matrix).transpose();
+   frontright.renderfast();
+
+   var backleft = new Cube();
+   backleft.color = wool;
+   backleft.textureNum = -2;
+   if(g_normalOn) backleft.textureNum = -3;
+   backleft.matrix.setTranslate(0, 0, 0);
+   backleft.matrix.rotate(-g_jointAngle, 0, 0, 1); // Joint 1
+   var backleftCoord = new Matrix4(backleft.matrix);
+   backleft.matrix.rotate(170, 0, 1, 0);
+   backleft.matrix.scale(.10, -0.10, 0.10);
+   backleft.matrix.translate(-1.15, -.25, 1.5);
+   backleft.normalMatrix.setInverseOf(backleft.matrix).transpose();
+   backleft.renderfast();
+
+   var backright = new Cube();
+   backright.color = wool;
+   backright.textureNum = -2;
+   if(g_normalOn) backright.textureNum = -3;
+   backright.matrix.setTranslate(0, 0, 0);
+   backright.matrix.rotate(g_jointAngle, 0, 0, 1); // Joint 1
+   var backrightCoord = new Matrix4(backright.matrix);
+   backright.matrix.rotate(170, 0, 1, 0);
+   backright.matrix.scale(.10, -0.10, 0.10);
+   backright.matrix.translate(.2, -.25, 1.5);
+   backright.normalMatrix.setInverseOf(backright.matrix).transpose();
+   backright.renderfast();
+
+
+
+   // lower leg =======================================
+   var frontleftlow = new Cube();
+   frontleftlow.color = skin;
+   if(g_normalOn) frontleftlow.textureNum = -3;
+   frontleftlow.matrix = frontleftCoord;
+   frontleftlow.matrix.rotate(170, 0, 1, 0);
+   frontleftlow.matrix.rotate(-g_jointAngle2, 0, 0, 1);
+   frontleftlow.matrix.scale(0.08, 0.08, 0.08);
+   frontleftlow.matrix.translate(-1.25, -1.75, -.8);
+   frontleftlow.normalMatrix.setInverseOf(frontleftlow.matrix).transpose();
+   frontleftlow.renderfast();
+
+   var frontrightlow = new Cube();
+   frontrightlow.color = skin;
+   if(g_normalOn) frontrightlow.textureNum = -3;
+   frontrightlow.matrix = frontrightCoord;
+   frontrightlow.matrix.rotate(170, 0, 1, 0);
+   frontrightlow.matrix.rotate(g_jointAngle2, 0, 0, 1);
+   frontrightlow.matrix.scale(0.08, 0.08, 0.08);
+   frontrightlow.matrix.translate(.37, -1.75, -.8);
+   frontrightlow.normalMatrix.setInverseOf(frontrightlow.matrix).transpose();
+   frontrightlow.renderfast();
+
+   var backleftlow = new Cube();
+   backleftlow.color = skin;
+   if(g_normalOn) backleftlow.textureNum = -3;
+   backleftlow.matrix = backleftCoord;
+   backleftlow.matrix.rotate(170, 0, 1, 0);
+   backleftlow.matrix.rotate(-g_jointAngle2, 0, 0, 1);
+   backleftlow.matrix.scale(0.08, 0.08, 0.08);
+   backleftlow.matrix.translate(-1.25, -1.75, 2);
+   backleftlow.normalMatrix.setInverseOf(backleftlow.matrix).transpose();
+   backleftlow.renderfast();
+
+   var backrightlow = new Cube();
+   backrightlow.color = skin;
+   if(g_normalOn) backrightlow.textureNum = -3;
+   backrightlow.matrix = backrightCoord;
+   backrightlow.matrix.rotate(170, 0, 1, 0);
+   backrightlow.matrix.rotate(g_jointAngle2, 0, 0, 1);
+   backrightlow.matrix.scale(0.08, 0.08, 0.08);
+   backrightlow.matrix.translate(.37, -1.75, 2);
+   backrightlow.normalMatrix.setInverseOf(backrightlow.matrix).transpose();
+   backrightlow.renderfast();
+
+}
+
+function drawAllShapes(){
+   // Pass light pos to GLSL
+   gl.uniform3f(u_lightPos, g_lightPos[0], g_lightPos[1], g_lightPos[2]);
+   gl.uniform3f(u_cameraPos, g_camera.eye.elements[0], g_camera.eye.elements[1], g_camera.eye.elements[2]);
+   gl.uniform1i(u_lightOn, g_lightOn);
+
+   // Draw Map=================================
+   // drawMap();
+
+   // Draw Sheep===============================
+   drawSheep();
+
+   // Sphere ==================================
+   var sphere = new Sphere();
+   sphere.color = [.9, .6, .95, 1];
+   sphere.textureNum = 0;
+   if(g_normalOn) sphere.textureNum = -3;
+   sphere.matrix.scale(.5, .5, .5);
+   sphere.matrix.translate(3, .75, -1.25);
+   sphere.render();
+
+   // Point Light ==================================
+
+   var light = new Cube();
+   light.color=[2,2,0,1];
+   light.matrix.translate(g_lightPos[0], g_lightPos[1],g_lightPos[2]);
+   light.matrix.scale(-.1,-.1,-.1);
+   light.matrix.translate(-.5, -.5,-.5);
+   light.renderfast();
+
+   // Sky =====================================
+   var sky = new Cube();
+   sky.color = [.6, .9, .95, 1];
+   // sky.textureNum = 1;
+   if(g_normalOn) sky.textureNum = -3;
+   sky.matrix.scale(-10,-10,-10);
+   sky.matrix.translate(-.5, -.5, -.5);
+   sky.render();
+
+
+   // Floor ===================================
+   var floor = new Cube();
+   floor.color = [.2, .9, .4, 1];
+   floor.textureNum = 1;
+   floor.matrix.translate(0,-.25,0);
+   floor.matrix.scale(10,0,10);
+   floor.matrix.translate(-.5, 0, -.5);
+   floor.render();
+
+}
